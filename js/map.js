3e4ab5161f153f3ac5e552ba65466fcf2f4678a3
2hausarzt2 <45627915+2hausarzt2@users.noreply.github.com>
2018-12-05 19:08:52 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@
         // Styles a map in night mode.
         var map = new google.maps.Map(document.getElementById('map'), {
           center:  {lat: 53.5573, lng: 9.97165},
-          zoom: 16,
+          zoom: 18,
           
 		  
 		  styles:  [{"featureType":"administrative.country","elementType":"geometry.fill","stylers":[{"color":"#61acc6"}]},
